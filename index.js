38dd0125631e877237eab26627ab7852733cf2c0
1donhxh <157998670+1donhxh@users.noreply.github.com>
2024-11-25 21:02:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,1 @@
-function toggleDropdown(day) {
-  var content = document.getElementById(day);
-  var button = content.previousElementSibling;
 
-  // Toggle the 'active' class on the button to trigger the dropdown visibility
-  button.classList.toggle('active');
-  content.style.display = content.style.display === "block" ? "none" : "block";
-}

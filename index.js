83914830829f50e08b37c2e06e97e13dd0d4d822
1donhxh <157998670+1donhxh@users.noreply.github.com>
2024-11-25 21:01:35 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
-function openTab(event, tabId) {
-    // Hide all tab content
-    const tabContents = document.querySelectorAll(".tab-content");
-    tabContents.forEach(content => content.classList.remove("active"));
-    
-    // Remove active class from all tab buttons
-    const tabButtons = document.querySelectorAll(".tab-button");
-    tabButtons.forEach(button => button.classList.remove("active"));
-    
-    // Show the clicked tab content and activate the clicked tab button
-    document.getElementById(tabId).classList.add("active");
-    event.currentTarget.classList.add("active");
-  }
-  
+function toggleDropdown(day) {
+  var content = document.getElementById(day);
+  var button = content.previousElementSibling;
+
+  // Toggle the 'active' class on the button to trigger the dropdown visibility
+  button.classList.toggle('active');
+  content.style.display = content.style.display === "block" ? "none" : "block";
+}
